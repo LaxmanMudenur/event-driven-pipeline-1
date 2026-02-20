29299317f9c72399bf44d59f1127aa3b58f8590a
LaxmanMudenur <150505081+LaxmanMudenur@users.noreply.github.com>
2026-02-20 12:28:02 +0530

    Implement S3 data ingestion in Lambda function

---
+++ data_ingest_lambda_1.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+from datetime import datetime
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    
+    data = event
+    
+    file_name = f"data/{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.json"
+    
+    s3.put_object(
+        Bucket="event-data-bucket-lucky-cf",
+        Key=file_name,
+        Body=json.dumps(data)
+    )
+    
+    return {
+        "statusCode": 200,
+        "body": "Event Data stored in S3!"
+    }
